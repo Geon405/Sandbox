3297f7b864487a7953f505a1733198971b453d53
Geon Kim <kg000405@gmail.com>
2020-02-24 11:06:30 -0500

    Improved sandbox

--- src/sandbox/Sandbox.java
+++ src/sandbox/Sandbox.java
@@ -30,6 +30,64 @@ public class Sandbox {
         System.out.println("Hello my sandbox in NetBeans!!!!");
         System.out.println("My first change to be saved to git.");
 
+        int a = 5;
+        int b = 5;
+        
+        System.out.println();
+        System.out.println("Equality with: a!=b " + (a!=b));
+        System.out.println("Equality with: !(a==b) " + !(a==b));
+        
+        System.out.println();
+        System.out.println("Relational: a<b " + (a<b));
+        System.out.println("Relational: a<=b " + (a<=b));
+        System.out.println("Relational: a>b " + (a>b));
+        System.out.println("Relational: a>=b " + (a>=b));
+
+        System.out.println();
+        System.out.println("Logical: !(a<=b) " + !(a<=b));
+        System.out.println("Logical: T && T " + ((a==b) && true));
+        System.out.println("Logical: T && F " + (true && false));
+        System.out.println("Logical: F && T " + ((a!=b) && true));
+        System.out.println("Logical: T || T " + (true  || true));
+        System.out.println("Logical: T || F " + (true  || false));
+        System.out.println("Logical: F || F " + (false  || false));
+        // Forming Condition
+        
+        System.out.println();
+        a = 2;
+        
+        if (a<b){
+            System.out.println("a is less than b");
+            if (a==2)
+            System.out.println("a is equal 2");
+            System.out.println("a is equal 2");
+        } else if (a<0) {
+            System.out.println("a is less than zero");
+        } else {
+            System.out.println("a is NOT less than b nor than zero");
+        }
+        System.out.println("Outside the if");
+        // if and else
+        
+        System.out.println();
+        
+        System.out.println("Conditional operator");
+        String result;
+        
+        result = (a==b)?"Yes, of course it is TRUE":"FALSE";
+        
+        /*if (a==b) {
+            result = "Yes, of course it is TRUE";
+        } else {
+            result = "FALSE";
+        }
+        */
+        System.out.println("Is a equal b? " + (a==b));
+        System.out.println("Is a equal b? " + result);
+        // Conditional operator (?:)
     }
-    
 }
+    
+
+
+
